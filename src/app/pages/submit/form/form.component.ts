a0a023ca34bb2aad56d034b7992dcc70550d3b69
victeo <victor.atomo@gmail.com>
2024-01-15 17:54:10 -0300

    incrementos do estilo

--- src/app/pages/submit/form/form.component.ts
+++ src/app/pages/submit/form/form.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnectionService } from '../../../services/firebase/connection.service';
 import { FormsModule } from '@angular/forms'; // Importe FormsModule
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 
 @Component({
     selector: 'app-form',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
     templateUrl: './form.component.html',
     styleUrl: './form.component.less'
 })
---
+++ src/app/theme.ts
@@ -0,0 +1,16 @@
+import { MatThemeModule } from '@angular/material/core';
+
+export const theme: MatThemeModule = {
+  palette: {
+    primary: {
+      main: '#0000FF',
+    },
+    secondary: {
+      main: '#FF0000',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+  },
+};
